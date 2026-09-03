c9c8f800c8b26b483ecad3e1edcc8aa37be1e556
Muhammad Ibraheem <codewithibf@gmail.com>
2026-09-03 23:36:00 +0500

    Add SafeAreaProvider to RootLayout component

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -3,6 +3,7 @@ import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
+
 export default function RootLayout() {
   return (
     <SafeAreaProvider>
